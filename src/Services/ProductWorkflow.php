ba75dbc2b7f13b0e1a4155876d4c61d89ce5076c
Martin Komischke <martin.komischke@gmail.com>
2021-03-08 20:04:10 +0100

    reorder methods

--- src/Services/ProductWorkflow.php
+++ src/Services/ProductWorkflow.php
@@ -18,6 +18,18 @@ class ProductWorkflow
         $this->workflow = $workflow;
     }
 
+    public function setNext(Product $product)
+    {
+        switch ($product->getStatus())
+        {
+            case 'prototyped': $this->implement($product); break;
+            case 'implemented': $this->test($product); break;
+            case 'tested': $this->wrap($product); break;
+            case 'wrapped': $this->ship($product); break;
+            default: throw new Exception('There is no next status after: ' . $product->getStatus() . '!');
+        }
+    }
+
     private function ship(Product $product)
     {
         $this->workflow->apply($product, 'ship');
@@ -36,17 +48,5 @@ class ProductWorkflow
     private function implement(Product $product)
     {
         $this->workflow->apply($product, 'implement');
-    }
-
-    public function setNext(Product $product)
-    {
-        switch ($product->getStatus())
-        {
-            case 'prototyped': $this->implement($product); break;
-            case 'implemented': $this->test($product); break;
-            case 'tested': $this->wrap($product); break;
-            case 'wrapped': $this->ship($product); break;
-            default: throw new Exception('There is no next status after: ' . $product->getStatus() . '!');
-        }
     }
 }
